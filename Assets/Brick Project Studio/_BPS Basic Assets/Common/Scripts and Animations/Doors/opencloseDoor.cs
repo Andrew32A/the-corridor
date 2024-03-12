61164b52d7c140f4b8f6a02f437515f6ffc4d5b1
Andrew Alsing <andrew32alsing@gmail.com>
2024-03-12 16:07:51 -0700

    added popup interact text for doors and fixed inverted entrance door bug

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -1,72 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-namespace SojaExiles
-
+public class opencloseDoor : MonoBehaviour
 {
-	public class opencloseDoor : MonoBehaviour
+	public Animator openandclose;
+	public bool open;
+	public Transform Player;
+	private TextMeshProUGUI interactText;
+	private bool playerInRange = false;
+
+	void Start()
 	{
-
-		public Animator openandclose;
-		public bool open;
-		public Transform Player;
-
-		void Start()
+		// open = false;
+		GameObject interactTextObject = GameObject.FindWithTag("InteractText");
+		if (interactTextObject != null)
 		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
+			interactText = interactTextObject.GetComponent<TextMeshProUGUI>();
+			if (interactText == null)
 			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
+				Debug.LogError("No TextMeshProUGUI component found on the object with tag 'InteractText'.");
 			}
-
 		}
-
-		IEnumerator opening()
+		else
 		{
-			print("you are opening the door");
-			openandclose.Play("Opening");
-			open = true;
-			yield return new WaitForSeconds(.5f);
+			Debug.LogError("No object found with tag 'InteractText'.");
 		}
 
-		IEnumerator closing()
+		if (interactText != null)
 		{
-			print("you are closing the door");
-			openandclose.Play("Closing");
-			open = false;
-			yield return new WaitForSeconds(.5f);
+			interactText.enabled = false;
 		}
+	}
 
+	void Update()
+	{
+		if (playerInRange && Player)
+		{
+			float dist = Vector3.Distance(Player.position, transform.position);
+			if (dist < 3.5f)
+			{
+				ShowInteractText(true);
 
+				if (Input.GetMouseButtonDown(0))
+				{
+					StartCoroutine(open ? closing() : opening());
+				}
+			}
+			else
+			{
+				ShowInteractText(false);
+			}
+		}
+	}
+
+	void OnMouseEnter()
+	{
+		playerInRange = true;
+	}
+
+	void OnMouseExit()
+	{
+		playerInRange = false;
+		ShowInteractText(false);
+	}
+
+	void ShowInteractText(bool show)
+	{
+		if (interactText != null)
+		{
+			interactText.enabled = show;
+		}
+	}
+
+	IEnumerator opening()
+	{
+		print("You are opening the door");
+		openandclose.Play("Opening");
+		open = true;
+		yield return new WaitForSeconds(.5f);
+	}
+
+	IEnumerator closing()
+	{
+		print("You are closing the door");
+		openandclose.Play("Closing");
+		open = false;
+		yield return new WaitForSeconds(.5f);
 	}
 }
