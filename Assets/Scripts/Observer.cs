0da7f36c06d8445440034cc64fe657fb2edfcfaf
Andrew Alsing <andrew32alsing@gmail.com>
2024-03-12 20:14:35 -0700

    added basic observer and lowered spot light intensity to 0.2 for all lights

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    [Header("Player")]
+    public GameObject player;
+
+    [Header("Doors")]
+    public GameObject[] doors;
+
+    [Header("Lights")]
+    public GameObject[] lights;
+
+    [Header("Teleporters")]
+    public GameObject[] teleporters;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
